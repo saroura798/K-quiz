d56a0e02738b293d770fda072f04b1dfa08d6f88
saroura798 <saroura798@users.noreply.github.com>
2025-12-16 15:55:23 +0000

    deploy: dd645dbab5de14b969d196dd1b872c206dca4033

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
